516ef96de97210b1fe445c20352842acc133edc6
koraband <97937113+koraband@users.noreply.github.com>
2024-10-13 18:09:11 +0530

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,40 @@
+# This is a simple Python program to print a message to the screen
+print("hello world")  # The print() function outputs the string "hello world" to the console
+#output is : hello world
+
+
+# DATA TYPES IN PYTHON
+ 
+# 1. integer(int)
+# Assign the value 10 to the variable 'a'
+a = 10  
+
+# The print() function outputs three things:
+# 1. The value of 'a' (which is 10)
+# 2. The type of 'a' (which is <class 'int'> because 'a' is an integer)
+# 3. The unique identifier (memory address) of the object referenced by 'a'
+print(a, type(a), id(a))
+# output is : 10 <class 'int'> 140733056223960
+
+# 2. float(float)
+# Assign the value 5.9 to the variable 'a'
+a = 5.9  
+
+# The print() function outputs three things:
+# 1. The value of 'a' (which is 5.9)
+# 2. The type of 'a' (which is <class 'float'> because 'a' is a floating-point number)
+# 3. The unique identifier (memory address) of the object referenced by 'a'
+print(a, type(a), id(a))
+# output is : 5.9 <class 'float'> 2000740303664
+
+# 3. bollean(bool)
+# true means = 1
+# false means = 0
+# Assign the boolean value True to the variable 'a'
+a = True  
+
+# The print() function outputs three things:
+# 1. The value of 'a' (which is True)
+# 2. The type of 'a' (which is <class 'bool'> because 'a' is a boolean)
+# 3. The unique identifier (memory address) of the object referenced by 'a'
+print(a, type(a), id(a))
